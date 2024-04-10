dcf25ea7092d78a61a371a98c54837bfb7a1e0fe
Eunhak0401 <74fresh4490@gmail.com>
2024-04-10 23:53:33 +0900

    do while

--- c/for.c
+++ c/for.c
@@ -1,9 +1,9 @@
 // 반복문
 // for, while, do while
-
+/*
 #include <stdio.h>
     void main()
-    /*
+  
     {
         int i;
         for (int i = 1; i <= 10; i++)
@@ -11,7 +11,7 @@
             printf("%d hello world\n", i);
         }
     }
-*/
+
 
 //while (조건)
 {
@@ -25,3 +25,47 @@ if (i > 5)
 
 }
 
+
+#include <stdio.h>
+    void main(){
+        int i;
+        // 1. 루프의 초기값
+        // 2. 종결 컨디션
+        // 3. 내용을 실행
+        // 4. 이후에 어떤 연산을 할건지 
+
+        for(i=10; i>0; i--){
+            printf("hello world %d\n", i);
+        }
+    }
+    
+
+#include <stdio.h>
+    void main(){
+        int i;
+
+        i = 0;
+        while (i<10)
+        {
+            printf("hello world %d\n", i);
+            i++;
+        }
+        
+    }
+*/
+#include <stdio.h>
+    void main(){
+        int i;
+
+        i = 0;  
+        do{
+            printf("hello world %d\n", i);
+            i++;
+        }while (i<10);
+    }
+        
+            
+        
+        
+        
+           
